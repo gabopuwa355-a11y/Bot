e2d495df4f2f70375da43b769fe772ba1ebe71e8
gabopuwa355-a11y <gabopuwa355@gmail.com>
2026-02-20 17:07:10 +0530

    Remove last_name from registrations insert statement
    Removed last_name from the registrations table insert statement.

--- main.py
+++ main.py
@@ -2236,7 +2236,7 @@ async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     cur.execute("""
     INSERT INTO registrations(
-        user_id, first_name, last_name, email, password, created_at, state
+        user_id, first_name, email, password, created_at, state
     ) VALUES(?,?,?,?,?,?)
     """, (
         user.id,
